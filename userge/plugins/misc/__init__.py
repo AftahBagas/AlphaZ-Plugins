5ef43ee827227a5bb6ced7dd0ce7b8ee461b9e4a
Koala <72527434+Camel07@users.noreply.github.com>
2021-02-01 22:01:21 +0800

    Update __init__.py

--- userge/plugins/misc/__init__.py
+++ userge/plugins/misc/__init__.py
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
 #
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# This file is part of < https://github.com/Camel07/KampangUsergay > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
 #
