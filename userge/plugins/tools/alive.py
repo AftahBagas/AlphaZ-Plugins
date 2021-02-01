0fff405a0dac3103f011e1dbb20b26f2a68561cd
Koala <72527434+Camel07@users.noreply.github.com>
2021-02-01 20:10:19 +0800

    Update alive.py

--- userge/plugins/tools/alive.py
+++ userge/plugins/tools/alive.py
@@ -64,25 +64,25 @@ def _get_mode() -> str:
 
 def _get_alive_text_and_markup(message: Message) -> Tuple[str, Optional[InlineKeyboardMarkup]]:
     markup = None
-    output = f"""KampangUsergay siap betumbuk..\n
-╭▻►▻►▻►▻►▻►◄◅◄◅◄◅◄◅╮\n
-• **⏱ Uptime** : `{userge.uptime}`⚡
-• **🌐 Version** : `{get_version()}`⚡
-• **🐨 Mode** : `{_get_mode().upper()}`⚡
+    output = f"""X-USERGE is running..\n
+▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n
+**⌚ uptime** : `{userge.uptime}`
+**🛠️ version** : `{get_version()}`
+**⚙️ Mode** : `{_get_mode().upper()}`
 \n╭━━━━━━━━━━━━━━━━╮
-• **👥 Sudo**: `{_parse_arg(Config.SUDO_ENABLED)}`⚡
-• **🗳️ Pm-Guard**: `{_parse_arg(not Config.ALLOW_ALL_PMS)}`⚡
-• **🚫 Anti-Spam**: `{_parse_arg(Config.ANTISPAM_SENTRY)}`⚡"""
+┣[• **Sudo**: `{_parse_arg(Config.SUDO_ENABLED)}`🔥
+┣[• **Pm-Guard**: `{_parse_arg(not Config.ALLOW_ALL_PMS)}`🔥
+┣[• **Anti-Spam**: `{_parse_arg(Config.ANTISPAM_SENTRY)}`🔥"""
     if Config.HEROKU_APP:
-        output += f"\n• **🧿 Dyno-saver**: `{_parse_arg(Config.RUN_DYNO_SAVER)}`⚡"
+        output += f"\n┣[• **Dyno-saver**: `{_parse_arg(Config.RUN_DYNO_SAVER)}`🔥"
     output += f"""
-• **🚀 Unofficial**: `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`⚡
+┣[• **Unofficial**: `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`🔥
 ╰━━━━━━━━━━━━━━━━╯\n
-    🐍**__Python__**: `{versions.__python_version__}`
-    💻**__Pyrogram__**: `{versions.__pyro_version__}`
-    \n╰▻►▻►▻►▻►▻►◄◅◄◅◄◅◄◅╯"""
-if not message.client.is_bot:
-        output += f"""
+  ⚙️**__Python__**: `{versions.__python_version__}`
+  ⚙️**__Pyrogram__**: `{versions.__pyro_version__}`
+\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱"""
+    if not message.client.is_bot:
+        output += f"""\n
 🎖 **{versions.__license__}** | 👥 **{versions.__copyright__}** | 🧪 **[Repo]({Config.UPSTREAM_REPO})**
 """
     else:
