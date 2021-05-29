a6232639f597382a70b8655686d89b297235252a
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-29 21:19:18 +0700

    Update versions.py

--- userge/versions.py
+++ userge/versions.py
@@ -17,5 +17,5 @@ __minor__ = 3
 __micro__ = 2
 
 __python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
-__license__ = "[GNU GPL v3.0](https://github.com/Camel07/KampangUsergay/blob/alpha/LICENSE)"
-__copyright__ = "[KampangUsergay](https://github.com/Camel07)"
+__license__ = "[GNU GPL v3.0](https://github.com/AftahBagas/AlphaZ-Plugins/blob/alpha/LICENSE)"
+__copyright__ = "[AlphaZ Plugins](https://github.com/AftahBagas)"
