d9fc78bdede6d2d69425abc5c3a1c2e38781fe0c
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-29 21:25:38 +0700

    Update covid.py

--- userge/plugins/utils/covid.py
+++ userge/plugins/utils/covid.py
@@ -13,18 +13,18 @@ from userge import userge, Message, pool
 
 
 @userge.on_cmd("covid", about={
-    'header': "see covid details",
-    'description': "The current real time situation of the COVID-19 patients reported in worldwide",
-    'flags': {'-l': "list countries"},
+    'header': "lihat detail yang jelas",
+    'description': "Situasi real time pasien COVID-19 saat ini dilaporkan di seluruh dunia",
+    'flags': {'-l': "daftar negara"},
     'usage': "{tr}covid [flag | country]",
     'examples': ["{tr}covid -l", "{tr}covid", "{tr}covid india"]})
 async def covid(message: Message):
-    await message.edit("`fetching covid ...`")
+    await message.edit("`mengambil covid ...`")
     covid_ = await pool.run_in_thread(Covid)("worldometers")
     country = message.input_str
     result = ""
     if '-l' in message.flags:
-        result += "<u>Covid Supported Countries</u>\n\n`"
+        result += "<u>Negara-negara yang Didukung Cmd Covid</u>\n\n`"
         result += '` , `'.join(sorted(filter(lambda x: x, covid_.list_countries())))
         result += "`"
     elif country:
@@ -33,23 +33,23 @@ async def covid(message: Message):
         except ValueError:
             await message.err(f"invalid country name <{country}>!")
             return
-        result += f"<u>Covid Status for {data['country']}</u>\n\n"
-        result += f"**new cases** : `{data['new_cases']}`\n"
-        result += f"**new deaths** : `{data['new_deaths']}`\n\n"
-        result += f"**critical** : `{data['critical']}`\n"
-        result += f"**active** : `{data['active']}`\n"
-        result += f"**confirmed** : `{data['confirmed']}`\n"
-        result += f"**deaths** : `{data['deaths']}`\n"
-        result += f"**recovered** : `{data['recovered']}`\n\n"
-        result += f"**total tests** : `{data['total_tests']}`\n"
-        result += f"**total tests per million** : `{data['total_tests_per_million']}`\n"
-        result += f"**total cases per million** : `{data['total_cases_per_million']}`\n"
-        result += f"**total deaths per million** : `{data['total_deaths_per_million']}`\n"
-        result += f"**population** : `{data['population']}`\n"
+        result += f"<u>Status Covid untuk {data['country']}</u>\n\n"
+        result += f"**kasus baru** : `{data['new_cases']}`\n"
+        result += f"**kematian baru** : `{data['new_deaths']}`\n\n"
+        result += f"**kritis** : `{data['critical']}`\n"
+        result += f"**aktif** : `{data['active']}`\n"
+        result += f"**dikonfirmasi** : `{data['confirmed']}`\n"
+        result += f"**meninggal** : `{data['deaths']}`\n"
+        result += f"**pulih** : `{data['recovered']}`\n\n"
+        result += f"**tes total** : `{data['total_tests']}`\n"
+        result += f"**total tes per juta** : `{data['total_tests_per_million']}`\n"
+        result += f"**total kasus per juta** : `{data['total_cases_per_million']}`\n"
+        result += f"**total kematian per juta** : `{data['total_deaths_per_million']}`\n"
+        result += f"**populasi** : `{data['population']}`\n"
     else:
-        result += "<u>Covid Status in the world</u>\n\n"
-        result += f"**total active cases** : `{covid_.get_total_active_cases()}`\n"
-        result += f"**total confirmed cases** : `{covid_.get_total_confirmed_cases()}`\n"
-        result += f"**total deaths** : `{covid_.get_total_deaths()}`\n"
-        result += f"**total recovered** : `{covid_.get_total_recovered()}`\n"
+        result += "<u>Status Covid di dunia</u>\n\n"
+        result += f"**total kasus aktif** : `{covid_.get_total_active_cases()}`\n"
+        result += f"**total kasus yang dikonfirmasi** : `{covid_.get_total_confirmed_cases()}`\n"
+        result += f"**total kematian** : `{covid_.get_total_deaths()}`\n"
+        result += f"**total pulih** : `{covid_.get_total_recovered()}`\n"
     await message.edit_or_send_as_file(result)
