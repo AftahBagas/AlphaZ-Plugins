4d88039be39854c4eb680753f2aeee6198053239
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-29 21:15:14 +0700

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,8 @@
 # pylint: disable=missing-module-docstring
 #
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/AlphaZ-Plugins >.
 #
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# This file is part of < https://github.com/AftahBagas/AlphaZ-Plugins > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
 #
