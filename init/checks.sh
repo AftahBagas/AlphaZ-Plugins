d467636cb66ca534836923604622212d6ee8abaa
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-30 05:53:45 +0700

    Update checks.sh

--- init/checks.sh
+++ init/checks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+# Copyright (C) 2020-2021 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
 #
 # This file is part of < https://github.com/UsergeTeam/Userge > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+# Please see < https://github.com/UsergeTeam/Userge/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -33,7 +33,6 @@ _checkConfigFile() {
             && quit "Please remove the line mentioned in the first hashtag from the config.env file"
     fi
 }
-
 _checkRequiredVars() {
     log "Checking Required ENV Vars ..."
     for var in API_ID API_HASH LOG_CHANNEL_ID DATABASE_URL; do
@@ -43,7 +42,6 @@ _checkRequiredVars() {
     [[ -n $BOT_TOKEN && -z $OWNER_ID ]] && quit "Required OWNER_ID var !"
     test -z $BOT_TOKEN && log "\t[HINT] >>> BOT_TOKEN not found ! (Disabling Advanced Loggings)"
 }
-
 _checkDefaultVars() {
     replyLastMessage "Checking Default ENV Vars ..."
     declare -rA def_vals=(
@@ -52,7 +50,8 @@ _checkDefaultVars() {
         [DOWN_PATH]="downloads"
         [UPSTREAM_REMOTE]="upstream"
         [UPSTREAM_REPO]="https://github.com/AftahBagas/AlphaZ-Plugins"
-        [LOAD_UNOFFICIAL_PLUGINS]=false
+        [LOAD_UNOFFICIAL_PLUGINS]=true
+        [CUSTOM_PLUGINS_REPO]=""
         [G_DRIVE_IS_TD]=true
         [CMD_TRIGGER]="."
         [SUDO_TRIGGER]="!"
@@ -70,8 +69,9 @@ _checkDefaultVars() {
         WORKERS=32
     fi
     export MOTOR_MAX_WORKERS=$WORKERS
+    export HEROKU_ENV=$(test $DYNO && echo 1 || echo 0)
     DOWN_PATH=${DOWN_PATH%/}/
-    if [[ -n $HEROKU_API_KEY && -n $HEROKU_APP_NAME ]]; then
+    if [[ $HEROKU_ENV == 1 && -n $HEROKU_API_KEY && -n $HEROKU_APP_NAME ]]; then
         local herokuErr=$(runPythonCode '
 import heroku3
 try:
@@ -80,7 +80,6 @@ try:
 except Exception as e:
     print(e)')
         [[ $herokuErr ]] && quit "heroku response > $herokuErr"
-        declare -g HEROKU_GIT_URL="https://api:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git"
     fi
     for var in G_DRIVE_IS_TD LOAD_UNOFFICIAL_PLUGINS; do
         eval $var=$(tr "[:upper:]" "[:lower:]" <<< ${!var})
@@ -111,7 +110,7 @@ _checkTriggers() {
 
 _checkPaths() {
     editLastMessage "Checking Paths ..."
-    for path in $DOWN_PATH logs bin; do
+    for path in $DOWN_PATH logs; do
         test ! -d $path && {
             log "\tCreating Path : ${path%/} ..."
             mkdir -p $path
@@ -119,68 +118,44 @@ _checkPaths() {
     done
 }
 
-_checkBins() {
-    editLastMessage "Checking BINS ..."
-    declare -rA bins=(
-        [bin/megadown]="https://raw.githubusercontent.com/yshalsager/megadown/master/megadown"
-        [bin/cmrudl]="https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py"
-    )
-    for bin in ${!bins[@]}; do
-        test ! -f $bin && {
-            log "\tDownloading $bin ..."
-            curl -so $bin ${bins[$bin]}
-        }
-    done
-}
-
-_checkGit() {
-    editLastMessage "Checking GIT ..."
-    if test ! -d .git; then
-        if test ! -z $HEROKU_GIT_URL; then
-            replyLastMessage "\tClonning Heroku Git ..."
-            gitClone $HEROKU_GIT_URL tmp_git || quit "Invalid HEROKU_API_KEY or HEROKU_APP_NAME var !"
-            mv tmp_git/.git .
-            rm -rf tmp_git
-            editLastMessage "\tChecking Heroku Remote ..."
-            remoteIsExist heroku || addHeroku
-        else
-            replyLastMessage "\tInitializing Empty Git ..."
-            gitInit
-        fi
-        deleteLastMessage
-    fi
-}
-
 _checkUpstreamRepo() {
-    editLastMessage "Checking UPSTREAM_REPO ..."
     remoteIsExist $UPSTREAM_REMOTE || addUpstream
-    replyLastMessage "\tFetching Data From UPSTREAM_REPO ..."
+    editLastMessage "Fetching Data From UPSTREAM_REPO ..."
     fetchUpstream || updateUpstream && fetchUpstream || quit "Invalid UPSTREAM_REPO var !"
     fetchBranches
     updateBuffer
-    deleteLastMessage
+}
+
+_setupPlugins() {
+    local link path tmp
+    if test $(grep -P '^'$2'$' <<< $3); then
+        editLastMessage "Cloning $1 Plugins ..."
+        link=$(test $4 && echo $4 || echo $3)
+        tmp=Temp-Plugins
+        gitClone --depth=1 $link $tmp
+        replyLastMessage "\tInstalling Requirements ..."
+        upgradePip
+        installReq $tmp
+        path=$(tr "[:upper:]" "[:lower:]" <<< $1)
+        rm -rf userge/plugins/$path/
+        mv $tmp/plugins/ userge/plugins/$path/
+        cp -r $tmp/resources/. resources/
+        rm -rf $tmp/
+        deleteLastMessage
+    else
+        editLastMessage "$1 Plugins Disabled !"
+    fi
 }
 
 _checkUnoffPlugins() {
-    editLastMessage "Checking UnOfficial Plugins ..."
-    if test $LOAD_UNOFFICIAL_PLUGINS = true; then
-        editLastMessage "\tLoading UnOfficial Plugins ..."
-        replyLastMessage "\t\tClonning ..."
-        gitClone --depth=1 https://github.com/AftahBagas/Alpha-Plugins.git
-        editLastMessage "\t\tUpgrading PIP ..."
-        upgradePip
-        editLastMessage "\t\tInstalling Requirements ..."
-        installReq Alpha-Plugins
-        editLastMessage "\t\tCleaning ..."
-        rm -rf userge/plugins/unofficial/
-        mv Alpha-Plugins/plugins/ userge/plugins/unofficial/
-        cp -r Alpha-Plugins/resources/* resources/
-        rm -rf Alpha-Plugins/
-        deleteLastMessage
-        editLastMessage "\tUnOfficial Plugins Loaded Successfully !"
-    else
-        editLastMessage "\tUnOfficial Plugins Disabled !"
-    fi
+    _setupPlugins Xtra true $LOAD_UNOFFICIAL_PLUGINS https://github.com/aftahbagas/Alpha-Plugins.git
+}
+
+_checkCustomPlugins() {
+    _setupPlugins Custom "https://([0-9a-f]{40}@)?github.com/.+/.+" $CUSTOM_PLUGINS_REPO
+}
+
+_flushMessages() {
     deleteLastMessage
 }
 
@@ -196,8 +171,8 @@ assertEnvironment() {
     _checkDatabase
     _checkTriggers
     _checkPaths
-    _checkBins
-    _checkGit
     _checkUpstreamRepo
     _checkUnoffPlugins
+    _checkCustomPlugins
+    _flushMessages
 }
