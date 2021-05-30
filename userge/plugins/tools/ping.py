b27154f8044cc22c6d5ace663e977c471f87e72f
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-30 07:36:09 +0700

    Update ping.py

--- userge/plugins/tools/ping.py
+++ userge/plugins/tools/ping.py
@@ -31,4 +31,4 @@ async def pingme(message: Message):
         await message.edit('`Alpha😈!`')
         end = datetime.now()
         m_s = (end - start).microseconds / 1000
-        await message.edit(f"**Alpha 😈!**\n**Pong!!** `{m_s} ms`\n**Uptime** : {uptime}" % (duration)`")
+        await message.edit(f"**Alpha 😈!**\n**Pong!!** `{m_s} ms`\n**Uptime**: `{userge.uptime}`")
