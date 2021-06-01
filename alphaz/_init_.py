eb85673d72c7255879a99facf68a194597fa0c6f
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 08:42:31 +0700

    Update and rename userge/__init__.py to alphaz/_init_.py

---
+++ alphaz/_init_.py
@@ -0,0 +1,8 @@
+# alfareza
+
+from alphaz.logger import logging  # noqa
+from alphaz.config import Config, get_version  # noqa
+from alphaz.core import (  # noqa
+    Alphaz, filters, Message, get_collection, pool)
+
+alphaz = Alphaz()  # userge is the client name
