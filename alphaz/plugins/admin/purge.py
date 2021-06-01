539a5a2d6c84474818430383e3da4297d1d4d94e
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 12:15:37 +0700

    Update and rename userge/plugins/admin/purge.py to alphaz/plugins/admin/purge.py

--- alphaz/plugins/admin/purge.py
+++ alphaz/plugins/admin/purge.py
@@ -1,17 +1,11 @@
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
+# alfareza
 
 from datetime import datetime
 
-from userge import userge, Message
+from alphaz import alphaz, Message
 
 
-@userge.on_cmd("purge", about={
+@alphaz.on_cmd("purge", about={
     'header': "purge messages from user",
     'flags': {
         '-u': "get user_id from replied message",
