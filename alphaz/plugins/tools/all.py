641a0fd24c8b9430978d13f6a49792d62028650b
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 14:42:35 +0700

    Update and rename userge/plugins/tools/all.py to alphaz/plugins/tools/all.py

---
+++ alphaz/plugins/tools/all.py
@@ -0,0 +1,13 @@
+# alfareza
+
+from alphaz import alphaz, Message
+from .. import get_all_plugins
+
+
+@alphaz.on_cmd("all", about={'header': "list all plugins in plugins/ path"})
+async def getplugins(message: Message):
+    raw_ = get_all_plugins()
+    out_str = f"**--({len(raw_)}) Plugins Available!--**\n\n"
+    for plugin in ('/'.join(i.split('.')) for i in raw_):
+        out_str += f"    `{plugin}.py`\n"
+    await message.edit(text=out_str, del_in=0)
