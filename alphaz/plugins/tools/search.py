8b68af81b023867d95432ecaa37143b9f5f14ac0
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 17:23:23 +0700

    Update and rename userge/plugins/tools/search.py to alphaz/plugins/tools/search.py

--- alphaz/plugins/tools/search.py
+++ alphaz/plugins/tools/search.py
@@ -1,16 +1,10 @@
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
+# alfareza
 
-from userge import userge, Message
+from alphaz import alphaz, Message
 
 
-@userge.on_cmd("s", about={
-    'header': "search commands in USERGE",
+@alphaz.on_cmd("s", about={
+    'header': "search commands in AlphaZ Plugins",
     'examples': "{tr}s wel"}, allow_channels=False)
 async def search(message: Message):
     cmd = message.input_str
