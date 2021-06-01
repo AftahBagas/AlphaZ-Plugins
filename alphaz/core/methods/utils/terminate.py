0f74e05a1e761b348440e362980b4cb0116e9047
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 10:54:37 +0700

    Update and rename userge/core/methods/utils/terminate.py to alphaz/core/methods/utils/terminate.py

--- alphaz/core/methods/utils/terminate.py
+++ alphaz/core/methods/utils/terminate.py
@@ -1,12 +1,4 @@
-# pylint: disable=missing-module-docstring
-#
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
+# alfareza
 
 __all__ = ['Terminate']
 
@@ -17,7 +9,7 @@ from ...ext import RawClient
 
 class Terminate(RawClient):  # pylint: disable=missing-class-docstring
     async def terminate(self) -> None:
-        """ terminate userge """
+        """ terminate alphaz """
         if not self.no_updates:
             for _ in range(self.workers):
                 self.dispatcher.updates_queue.put_nowait(None)
