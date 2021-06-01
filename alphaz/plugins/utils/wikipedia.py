beb8c69256a20691428e7c904ca66fccd65cc4a2
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 20:39:12 +0700

    Update and rename userge/plugins/utils/wikipedia.py to alphaz/plugins/utils/wikipedia.py

--- alphaz/plugins/utils/wikipedia.py
+++ alphaz/plugins/utils/wikipedia.py
@@ -1,21 +1,15 @@
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
+# alfareza
 
 import wikipedia
 
-from userge import userge, Message
+from alphaz import alphaz, Message
 
 
-@userge.on_cmd("wiki", about={
+@alphaz.on_cmd("wiki", about={
     'header': "do a Wikipedia search",
     'flags': {'-l': "limit the number of returned results (defaults to 5)"},
     'usage': "{tr}wiki [flags] [query | reply to msg]",
-    'examples': "{tr}wiki -l5 userge"})
+    'examples': "{tr}wiki -l5 AlphaZ Plugins"})
 async def wiki_pedia(message: Message):
     await message.edit("Processing ...")
     query = message.filtered_input_str
