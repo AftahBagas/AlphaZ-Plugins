7b9cb8a83a8ed850107ce85ce1b41cd465e35d38
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 11:52:32 +0700

    Update basic.sh

--- init/logbot/methods/basic.sh
+++ init/logbot/methods/basic.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-#
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
+# alfareza
 
 sendMessage() {
     test -z "$1" || api.sendMessage $LOG_CHANNEL_ID "$1"
