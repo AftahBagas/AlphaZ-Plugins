a2b241a9f6c3b167ba0fd7e65092d8d06cf0ceb5
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 19:42:46 +0700

    Update and rename userge/plugins/utils/google.py to alphaz/plugins/utils/google.py

--- alphaz/plugins/utils/google.py
+++ alphaz/plugins/utils/google.py
@@ -1,17 +1,11 @@
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
+# alfareza
 
 from search_engine_parser import GoogleSearch
 
-from userge import userge, Message
+from alphaz import alphaz, Message
 
 
-@userge.on_cmd("google", about={
+@alphaz.on_cmd("google", about={
     'header': "do a Google search",
     'flags': {
         '-p': "page of results to return (default to 1)",
