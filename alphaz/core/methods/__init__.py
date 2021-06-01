b0662f893b8374b5b525fa7a37c9b184f2dcb5af
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 09:37:08 +0700

    Update and rename userge/core/methods/__init__.py to alphaz/core/methods/__init__.py

---
+++ alphaz/core/methods/__init__.py
@@ -0,0 +1,13 @@
+# alfareza
+
+__all__ = ['Methods']
+
+from .chats import Chats
+from .decorators import Decorators
+from .messages import Messages
+from .users import Users
+from .utils import Utils
+
+
+class Methods(Chats, Decorators, Messages, Users, Utils):
+    """ alpha.methods """
