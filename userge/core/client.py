54441aa3c49c1736ab6a99b3132e73d9d0770a2f
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 09:24:43 +0700

    Update client.py

--- userge/core/client.py
+++ userge/core/client.py
@@ -1,14 +1,6 @@
-# pylint: disable=missing-module-docstring
-#
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
+# alfareza
 
-__all__ = ['Userge']
+__all__ = ['Alphaz']
 
 import time
 import signal
@@ -19,10 +11,10 @@ from typing import List, Awaitable, Any, Optional, Union
 
 from pyrogram import idle
 
-from userge import logging, Config, logbot
-from userge.utils import time_formatter
-from userge.utils.exceptions import UsergeBotNotFound
-from userge.plugins import get_all_plugins
+from alphaz import logging, Config, logbot
+from alphaz.utils import time_formatter
+from alphaz.utils.exceptions import AlphazBotNotFound
+from alphaz.plugins import get_all_plugins
 from .methods import Methods
 from .ext import RawClient, pool
 from .database import _close_db
@@ -52,7 +44,7 @@ class _AbstractUserge(Methods, RawClient):
 
     @property
     def uptime(self) -> str:
-        """ returns userge uptime """
+        """ returns alphaz plugins uptime """
         return time_formatter(time.time() - _START_TIME)
 
     async def finalize_load(self) -> None:
@@ -60,10 +52,10 @@ class _AbstractUserge(Methods, RawClient):
         await asyncio.gather(_complete_init_tasks(), self.manager.init())
 
     async def load_plugin(self, name: str, reload_plugin: bool = False) -> None:
-        """ Load plugin to Userge """
+        """ Load plugin to AlphaZ Plugins """
         _LOG.debug(_LOG_STR, f"Importing {name}")
         _IMPORTED.append(
-            importlib.import_module(f"userge.plugins.{name}"))
+            importlib.import_module(f"alphaz.plugins.{name}"))
         if reload_plugin:
             _IMPORTED[-1] = importlib.reload(_IMPORTED[-1])
         plg = _IMPORTED[-1]
@@ -104,10 +96,10 @@ class _AbstractUserge(Methods, RawClient):
         return len(reloaded)
 
 
-class UsergeBot(_AbstractUserge):
-    """ UsergeBot, the bot """
+class AlphazBot(_AbstractUserge):
+    """ AlphazBot, the bot """
     def __init__(self, **kwargs) -> None:
-        _LOG.info(_LOG_STR, "Setting UsergeBot Configs")
+        _LOG.info(_LOG_STR, "Setting AlphazBot Configs")
         super().__init__(session_name=":memory:", **kwargs)
 
     @property
@@ -116,13 +108,13 @@ class UsergeBot(_AbstractUserge):
         return self._bot
 
 
-class Userge(_AbstractUserge):
-    """ Userge, the userbot """
+class Alphaz(_AbstractUserge):
+    """ AlphaZ Plugins, the userbot """
 
     has_bot = bool(Config.BOT_TOKEN)
 
     def __init__(self, **kwargs) -> None:
-        _LOG.info(_LOG_STR, "Setting Userge Configs")
+        _LOG.info(_LOG_STR, "Setting AlphaZ Plugins Configs")
         kwargs = {
             'api_id': Config.API_ID,
             'api_hash': Config.API_HASH,
@@ -132,15 +124,15 @@ class Userge(_AbstractUserge):
             kwargs['bot_token'] = Config.BOT_TOKEN
         if Config.HU_STRING_SESSION and Config.BOT_TOKEN:
             RawClient.DUAL_MODE = True
-            kwargs['bot'] = UsergeBot(bot=self, **kwargs)
+            kwargs['bot'] = AlphazBot(bot=self, **kwargs)
         kwargs['session_name'] = Config.HU_STRING_SESSION or ":memory:"
         super().__init__(**kwargs)
         self.executor.shutdown()
         self.executor = pool._get()  # pylint: disable=protected-access
 
     @property
-    def bot(self) -> Union['UsergeBot', 'Userge']:
-        """ returns usergebot """
+    def bot(self) -> Union['AlphazBot', 'Alphaz']:
+        """ returns alphazbot """
         if self._bot is None:
             if Config.BOT_TOKEN:
                 return self
@@ -149,25 +141,25 @@ class Userge(_AbstractUserge):
 
     async def start(self) -> None:
         """ start client and bot """
-        _LOG.info(_LOG_STR, "Starting Userge")
+        _LOG.info(_LOG_STR, "Starting Alphaz")
         await super().start()
         if self._bot is not None:
-            _LOG.info(_LOG_STR, "Starting UsergeBot")
+            _LOG.info(_LOG_STR, "Starting AlphazBot")
             await self._bot.start()
         await self._load_plugins()
 
     async def stop(self) -> None:  # pylint: disable=arguments-differ
         """ stop client and bot """
         if self._bot is not None:
-            _LOG.info(_LOG_STR, "Stopping UsergeBot")
+            _LOG.info(_LOG_STR, "Stopping AlphazBot")
             await self._bot.stop()
-        _LOG.info(_LOG_STR, "Stopping Userge")
+        _LOG.info(_LOG_STR, "Stopping Alphaz")
         await super().stop()
         _close_db()
         pool._stop()  # pylint: disable=protected-access
 
     def begin(self, coro: Optional[Awaitable[Any]] = None) -> None:
-        """ start userge """
+        """ start alphaz """
         lock = asyncio.Lock()
         running_tasks: List[asyncio.Task] = []
 
@@ -187,7 +179,7 @@ class Userge(_AbstractUserge):
             _LOG.info(_LOG_STR, "Loop Stopped !")
 
         async def _shutdown(sig: signal.Signals) -> None:
-            _LOG.info(_LOG_STR, f"Received Stop Signal [{sig.name}], Exiting Userge ...")
+            _LOG.info(_LOG_STR, f"Received Stop Signal [{sig.name}], Exiting AlphaZ Plugins ...")
             await _finalize()
 
         for sig in (signal.SIGHUP, signal.SIGTERM, signal.SIGINT):
@@ -196,7 +188,7 @@ class Userge(_AbstractUserge):
         self.loop.run_until_complete(self.start())
         for task in self._tasks:
             running_tasks.append(self.loop.create_task(task()))
-        logbot.edit_last_msg("Userge has Started Successfully !")
+        logbot.edit_last_msg("AlphaZ Plugins has Started Successfully !")
         logbot.end()
         mode = "[DUAL]" if RawClient.DUAL_MODE else "[BOT]" if Config.BOT_TOKEN else "[USER]"
         try:
