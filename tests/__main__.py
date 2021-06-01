2ffb92ca7a984571d9852b8444c5695156afa1b1
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-01 11:25:35 +0700

    Update __main__.py

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,23 +1,15 @@
-# pylint: disable=missing-module-docstring
-#
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
+# alfareza
 
 import os
 
-from userge import userge
+from alphaz import alphaz
 
 
 async def _worker() -> None:
     chat_id = int(os.environ.get("CHAT_ID") or 0)
-    type_ = 'unofficial' if os.path.exists("../userge/plugins/unofficial") else 'main'
-    await userge.send_message(chat_id, f'`{type_} build completed !`')
+    type_ = 'unofficial' if os.path.exists("../alphaz/plugins/unofficial") else 'main'
+    await alphaz.send_message(chat_id, f'`{type_} build completed !`')
 
 if __name__ == "__main__":
-    userge.begin(_worker())
-    print('userge test has been finished!')
+    alphaz.begin(_worker())
+    print('alphaz test has been finished!')
